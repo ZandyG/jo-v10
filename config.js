a5c9358ea128b4dc7b1159d7f15c315f01bf2a2b
ZandyG <116550970+ZandyG@users.noreply.github.com>
2022-11-10 13:19:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,13 +17,13 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE JO BOT'
+global.ownername = 'ZANDY BOTZ'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
+global.owner = ['6285863702349','6288291577991','6285863702349']
+global.premium = ['6285863702349','6288291577991']
 global.packname = 'The Jo Bot'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
